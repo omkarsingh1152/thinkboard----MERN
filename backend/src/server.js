28b484b451ad8dfa0b16c845064d102feb8f5f2c
Omkar Singh <omkariitp@gmail.com>
2026-08-03 19:16:47 +0530

    Fix Render frontend static path

--- backend/src/server.js
+++ backend/src/server.js
@@ -77,7 +77,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.use(
         express.static(
-            path.join(__dirname, "frontend/thinkboard/dist")
+            path.join(__dirname, "../frontend/thinkboard/dist")
         )
     );
 
