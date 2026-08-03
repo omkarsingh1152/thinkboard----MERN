4320c33335f2d1729a6a52e67d5cd859f40435ea
Omkar Singh <omkariitp@gmail.com>
2026-08-03 18:41:55 +0530

    second

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,26 +4,38 @@ import connectDB from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 // console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 
-const app = express();
 
+
+const app = express();
+const __dirname = path.resolve();
 connectDB();
 
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL
-}));
-app.use(express.json());
-app.use(rateLimiter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(cors({
+        origin: process.env.FRONTEND_URL
+    }));
+    app.use(express.json());
+    app.use(rateLimiter);
+}
 
 
 
 app.use("/api/notes", notesRoutes);
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/thinkboard/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/thinkboard/build/index.html"));
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });}
--- frontend/thinkboard/src/lib/axios.js
+++ frontend/thinkboard/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 
+
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 const api = axios.create({
